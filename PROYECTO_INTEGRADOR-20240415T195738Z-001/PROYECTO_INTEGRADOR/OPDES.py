31415104ab8ad5de54202657c2b27bdc5c1eb2bc
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-07-05 23:51:00 -0600

    Perfil de usuario.
    Se muestra la información de el usuario, editar sus datos y poder agregar foto de portada y de perfil.

--- PROYECTO_INTEGRADOR-20240415T195738Z-001/PROYECTO_INTEGRADOR/OPDES.py
+++ PROYECTO_INTEGRADOR-20240415T195738Z-001/PROYECTO_INTEGRADOR/OPDES.py
@@ -3,18 +3,20 @@ from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
 from functools import wraps
+import os
 import hashlib
 import base64
 
-app= Flask(__name__,  template_folder='templates')
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='opdes'
-app.secret_key= 'mysecrety'
-Bcrypt=Bcrypt(app)
-mysql= MySQL(app)
+app = Flask(__name__, template_folder='templates')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'opdes'
+app.secret_key = 'mysecrety'
+bcrypt = Bcrypt(app)
+mysql = MySQL(app)
 
+#Ruta para logearse.
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -24,6 +26,9 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+
+
+#Ruta de roles.
 def role_required(roles):
     def decorator(f):
         @wraps(f)
@@ -36,24 +41,99 @@ def role_required(roles):
     return decorator
 
 
+
+#Ruta para almacenamiento de imagenes.
+def save_photo(file):
+    if file and file.filename:
+        return file.read()
+    return None
+
+
+#Ruta de inicializacion.
 @app.route('/')
 def Inicio():
     return render_template('Inicio.html')
 
 
-#Esta es la ruta para ir a la vista para el registro de un proyecto 
+
+#Ruta de Login.
+@app.route('/Login', methods=['GET', 'POST'])
+def Login():
+    if request.method == 'POST':
+        correo = request.form['correo']
+        contraseña = request.form['contraseña']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT ro.rol, contraseña FROM usuarios us INNER JOIN roles ro ON us.id_rol = ro.id WHERE us.correo = %s', (correo,))
+        usuario = cursor.fetchone()
+        cursor.close()
+
+        if usuario:
+            stored_password = usuario[1]
+            if bcrypt.check_password_hash(stored_password, contraseña):
+                session['correo'] = correo  
+                session['rol'] = usuario[0]
+                if usuario[0] == 'Publicador' or usuario[0] == 'Visualizador':
+                    return redirect(url_for('Visualizador'))
+                elif usuario[0] == 'Administrador':
+                    return redirect(url_for('Administrador'))
+                else:
+                    flash('Rol no válido', 'error')
+            else:
+                flash('Correo o contraseña incorrectos', 'error')
+        else:
+            flash('Correo o contraseña incorrectos', 'error')
+
+    return render_template('Login.html')
+
+
+
+# Ruta de registro de usuario.
+@app.route('/Registro')
+def Registro():
+    return render_template('Registrarse.html')
+
+@app.route('/guardarRegistro', methods=['POST'])
+def guardarRegistro():
+    if request.method == 'POST':
+        Vnombre = request.form['Nombre']
+        Vapellidos = request.form['Apellidos']
+        Vfecha_nac = request.form['Fecha_nac']
+        Vcorreo = request.form['Correo']
+        Vcontraseña = request.form['Contraseña']
+        Vrol = int(request.form['rol'])
+
+        hashed_password = bcrypt.generate_password_hash(Vcontraseña).decode('utf-8')
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO usuarios (nombre, apellidos, f_nacimiento, correo, contraseña, id_rol) VALUES (%s, %s, %s, %s, %s, %s)', 
+                       (Vnombre, Vapellidos, Vfecha_nac, Vcorreo, hashed_password, Vrol))
+        mysql.connection.commit()
+        cursor.close()
+
+        session['correo'] = Vcorreo
+        session['rol'] = Vrol
+
+        if Vrol == 1 or Vrol == 2:
+            return redirect(url_for('Visualizador'))
+        else:
+            flash('Error al registrar usuario', 'error')
+            return redirect(url_for('Registrarse'))
+
+    flash('Error al registrar usuario', 'error')
+    return redirect(url_for('Registrarse'))
+
+
+
+#Ruta de registro de proyectos.
 @app.route('/Registro_Proyecto')
 @login_required
 def Registro_Proyecto():
-    
     return render_template('Registro_Proyecto.html')
 
-
-#Esta es la ruta para guardar un proyecto en la base de datos
 @app.route('/guardarProyecto', methods=['POST'])
 @login_required
 def guardarProyecto():
-    
     if request.method == 'POST':
         Vnombre = request.form['nombre-proyecto']
         Vnombreempresa = request.form['nombre-empresa']
@@ -62,73 +142,41 @@ def guardarProyecto():
         Vdescripcion = request.form['descripcion-proyecto']
         Vobjetivo = request.form['objetivo-proyecto']
         
-        # Manejo del archivo de imagen
-        if 'file-upload' not in request.files:
-            return 'No file part'
-        file = request.files['file-upload']
-        if file.filename == '':
-            return 'No selected file'
-        if file:
+        file = request.files.get('file-upload')
+        if file and file.filename:
             imagen = file.read()
         else:
-            return 'File not allowed'
+            flash('No file selected or file not allowed')
+            return redirect(url_for('Registro_Proyecto'))
 
         cursor = mysql.connection.cursor()
         cursor.execute('INSERT INTO proyectos (nombre, nombre_empresa, correo_electronico, telefono, foto, descripcion, objetivo) VALUES (%s, %s, %s, %s, %s, %s, %s)', 
                     (Vnombre, Vnombreempresa, Vcorreo, Vtelefono, imagen, Vdescripcion, Vobjetivo))
         mysql.connection.commit()
         cursor.close()
+        flash('Project saved successfully!')
 
     return redirect(url_for('VisualizadorProyectos'))
 
 
-#ESTA RUTA ES PARA ADMINISTRADOR 
-@app.route('/Administrador')
-@login_required
-def Administrador():
-    return render_template('admin.html')
 
-
-@app.route('/AdminProyectos')
-@login_required
-def AdminProyectos():
-
-    return render_template('adminProyectos.html')
-
-
-@app.route('/AdminInicio')
-@login_required
-def AdminInicio():
-    
-    return render_template('adminInicio.html')
-
-
-#ESTA RUTA ES PARA EL ROL VISUALIZADOR DE PROYECTOS
+#Ruta de Inicio (¿Quienes somos?)
 @app.route('/Visualizador')
 @login_required
 def Visualizador():
-    
     return render_template('Visualizador.html')
 
 
-@app.route('/Visualizadorperfil')
-@login_required
-def VisualizadorPerfil():
-    
-    return render_template('VisualizadorPerfil.html')
 
-
-
+#Ruta para ver los proyectos.
 @app.route('/VisualizadorProyectos')
 @login_required
 def VisualizadorProyectos():
-
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT * FROM proyectos')
     proyectosBD = cursor.fetchall()
     cursor.close()
 
-    # Convierte las imágenes BLOB a base64 para poder ser renderizadas en HTML
     proyectos = [
         {
             'id': p[0],
@@ -144,142 +192,115 @@ def VisualizadorProyectos():
 
     return render_template('VisualizadorProyectos.html', proyectos=proyectos)
 
-    
 
+
+#Ruta de Perfil
+@app.route('/Perfil', methods=['GET', 'POST'])
+@login_required
+def Perfil():
+    if 'correo' not in session:
+        return redirect(url_for('Login'))
+
+    user_email = session.get('correo')
+
+    if request.method == 'POST':
+        # Process the form data
+        nombre = request.form['name']
+        correo = request.form['email']
+        ubicacion = request.form['location']
+        
+        foto_perfil = save_photo(request.files.get('profile-pic'))
+        foto_portada = save_photo(request.files.get('cover-pic'))
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('''
+            UPDATE usuarios 
+            SET nombre=%s, correo=%s
+            WHERE correo=%s
+        ''', (nombre, correo, user_email))
+
+       
+        if foto_perfil:
+            cursor.execute('''
+                UPDATE perfiles_usuarios 
+                SET foto_perfil=%s
+                WHERE id_usuario=(SELECT id FROM usuarios WHERE correo=%s)
+            ''', (foto_perfil, user_email))
+        
+        if foto_portada:
+            cursor.execute('''
+                UPDATE perfiles_usuarios 
+                SET foto_portada=%s
+                WHERE id_usuario=(SELECT id FROM usuarios WHERE correo=%s)
+            ''', (foto_portada, user_email))
+
+        cursor.execute('''
+            INSERT INTO perfiles_usuarios (id_usuario, ubicacion, foto_perfil, foto_portada)
+            VALUES ((SELECT id FROM usuarios WHERE correo=%s), %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                ubicacion=VALUES(ubicacion),
+                foto_perfil=COALESCE(VALUES(foto_perfil), foto_perfil),
+                foto_portada=COALESCE(VALUES(foto_portada), foto_portada)
+        ''', (user_email, ubicacion, foto_perfil, foto_portada))
+
+        mysql.connection.commit()
+        cursor.close()
+        flash('Perfil actualizado exitosamente.')
+        return redirect(url_for('Perfil'))
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('''
+        SELECT u.nombre, u.apellidos, u.correo, p.ubicacion, p.foto_perfil, p.foto_portada
+        FROM usuarios u
+        LEFT JOIN perfiles_usuarios p ON u.id = p.id_usuario
+        WHERE u.correo = %s
+    ''', (user_email,))
+    user_data = cursor.fetchone()
+    cursor.close()
+
+    if not user_data:
+        flash('Usuario no encontrado.', 'error')
+        return redirect(url_for('Login'))
+
+    user_profile_data = {
+        'nombre': user_data[0],
+        'apellidos': user_data[1],
+        'correo': user_data[2],
+        'ubicacion': user_data[3],
+        'foto_perfil': base64.b64encode(user_data[4]).decode('utf-8') if user_data[4] else None,
+        'foto_portada': base64.b64encode(user_data[5]).decode('utf-8') if user_data[5] else None
+    }
+
+    return render_template('perfil.html', user_data=user_profile_data)
+
+
+
+
+
+
+
+
+
+
+#FALTANTES
 @app.route('/VisualizadorNotificaciones')
 @login_required
 def VisualizadorNotificaciones():
-    
     return render_template('VisualizadorNotificaciones.html')
-
-
-
-@app.route('/VisualizadorMasProyectos')
-@login_required
-def VisualizadorMasProyectos():
-    
-    return render_template('VisualizadorMasProyectos.html')
-
-
-
-@app.route('/Login', methods=['GET', 'POST'])
-def Login():
-    if request.method == 'POST':
-        correo = request.form['correo']
-        contraseña = request.form['contraseña']
-
-        cursor = mysql.connection.cursor()
-        cursor.execute('SELECT ro.rol, contraseña FROM usuarios us inner join roles ro on us.id_rol = ro.id WHERE us.correo = %s', (correo,))
-        usuario = cursor.fetchone()
-        cursor.close()
-
-        if usuario:
-            stored_password = usuario[1]
-            if Bcrypt.check_password_hash(stored_password, contraseña):
-                session['rol'] = usuario[0]
-                if usuario[0] == 'Publicador' or usuario[0] == 'Visualizador':
-                    return redirect(url_for('Visualizador'))
-                elif usuario[0] == 'Administrador':
-                    return redirect(url_for('Administrador'))
-                else:
-                    flash('Rol no válido', 'error')
-                    return redirect(url_for('Login'))
-            else:
-                flash('Correo o contraseña incorrectos', 'error')  # Cambia la categoría a 'error'
-                return redirect(url_for('Login'))  
-            
-        else:
-            flash('Correo o contraseña incorrectos', 'error')  # Cambia la categoría a 'error'
-            return redirect(url_for('Login'))  
-
-    return render_template('Login.html')
-
-
-
-
-
-@app.route('/Registro')
-def Registro ():
-    return render_template('Registrarse.html')
-
-@app.route('/guardarRegistro', methods=['POST'])
-def guardarRegistro():
-    if request.method == 'POST':
-        Vnombre = request.form['Nombre']
-        Vapellidos = request.form['Apellidos']
-        Vfecha_nac = request.form['Fecha_nac']
-        Vcorreo = request.form['Correo']
-        Vcontraseña = request.form['Contraseña']
-        Vrol = int(request.form['rol'])  # Asegúrate de convertir a entero
-
-        hashed_password = Bcrypt.generate_password_hash(Vcontraseña).decode('utf-8')
-
-        cs = mysql.connection.cursor()
-        cs.execute('INSERT INTO usuarios (nombre, apellidos, f_nacimiento, correo, contraseña, id_rol) VALUES (%s, %s, %s, %s, %s, %s)', 
-                   (Vnombre, Vapellidos, Vfecha_nac, Vcorreo, hashed_password, Vrol))
-        mysql.connection.commit()
-        cs.close()
-        session['rol'] = Vrol
-        
-        # Redirige según el rol
-        if Vrol == 1 or Vrol == 2:
-            return redirect(url_for('Visualizador'))
-        else:
-            flash('Error al registrar usuario', 'error')
-            return redirect(url_for('Registro'))
-    
-    # Manejo del caso en que el método no sea POST
-    flash('Error al registrar usuario', 'error')
-    return redirect(url_for('Registro'))
-
-
-
-
-
-
-
-@app.route('/Inicio_pagina_Publicador')
-@login_required
-def Inicio_pagina():
-    if 'rol' not in session: 
-        return redirect(url_for('Login'))
-    return render_template('Inicio2.html')
-
-@app.route('/Proyectos')
-@login_required
-def Proyectos():
-    if 'rol' not in session: 
-        return redirect(url_for('Login'))
-    return render_template('proyectos.html')
-
-@app.route('/Perfil')
-@login_required
-def Perfil():
-    if 'rol' not in session: 
-        return redirect(url_for('Login'))
-    return render_template('perfil.html')
 
 @app.route('/Notificaciones')
 @login_required
 def Notificaciones():
-    if 'rol' not in session: 
-        return redirect(url_for('Login'))
     return render_template('Notificaciones.html')
 
 @app.route('/PerfilProyecto')
 @login_required
 def PerfilProyecto():
-    if 'rol' not in session: 
-        return redirect(url_for('Login'))
     return render_template('Proyectoperfil.html')
 
-
-#Esta ruta es para la interfaz de proyectos de otras personas
 @app.route('/Perfil_Proyectos')
 @login_required
 def Perfil_Proyectos():
-    if 'rol' not in session: 
-        return redirect(url_for('Login'))
     return render_template('Perfil_Proyectos.html')
 
 @app.route('/CerrarSesion')
@@ -287,7 +308,6 @@ def Perfil_Proyectos():
 def logout():
     session.clear()
     return redirect(url_for('Login'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
